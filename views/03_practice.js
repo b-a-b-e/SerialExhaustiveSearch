f8b085f67b605d3698d7b454c388979c419659d4
n-kall <33577035+n-kall@users.noreply.github.com>
2018-04-05 09:28:31 +0000

    fix timing variables

--- views/03_practice.js
+++ views/03_practice.js
@@ -14,21 +14,19 @@ var initPracticeView = function(CPT) {
 	stimulus: exp.data.practice_trials[CPT].stimulus,
 	probe: exp.data.practice_trials[CPT].probe
     }));
-    // attaches an event listener to the yes / no radio inputs
-    // when an input is selected a response property with a value equal to the answer is added to the trial object
-    // as well as a readingTimes property with value - a list containing the reading times of each word
-
-        var pause = 10;
+	
     // how long stimulus is shown for
     var showTime = 3000;
+    // time between stimulus and probe
+    var blinkTime = 1000;
     var dateStart, rt
     $('#probe').hide()
     setTimeout(function() {
         $('#stimulus').hide();
-    }, 1000); // <-- time in milliseconds
+    }, showTime); // <-- time in milliseconds
     setTimeout(function() {
         $('#probe').show();
-    }, 2000); // <-- time in milliseconds
+    }, showTime + blinkTime); // <-- time in milliseconds
 
     return view;
 };
