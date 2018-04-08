1b08e59aa9a3771482a6304c0cd444981551e183
n-kall <noa@tutamail.com>
2018-04-08 18:58:47 +0200

    typofix

--- views/01_intro.js
+++ views/01_intro.js
@@ -3,7 +3,7 @@ config_views.intro = {
     // introduction title
     "title": "Welcome!",
     // introduction text
-    "text": "Thank you for participating in our study. In this study, you will answer questions about 24 sentences that describe images. To participate in the experiment, please first accept the HIT. By answering the following questions, you are participating in a study being performed by scientists from the University of Osnabrück. You must be at least 18 years old to participate. Your participation in this research is voluntary. You may decline to answer any or all of the following questions. You may decline further participation, at any time, without adverse consequences. Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you. Press space to go to continue.",
+    "text": "Thank you for participating in our study. By answering the following questions, you are participating in a study being performed by scientists from the University of Osnabrück. You must be at least 18 years old to participate. Your participation in this research is voluntary. You may decline to answer any or all of the following questions. You may decline further participation, at any time, without adverse consequences. Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you. Press SPACE to accept and continue.",
     // introduction's slide proceeding button text
     "buttonText": "Begin experiment"
 };
