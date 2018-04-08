22cdf4059a9640e14737cafa85245cb8ee561f2f
n-kall <noa@tutamail.com>
2018-04-08 18:57:41 +0200

    change number of trials to 144

--- config/config_general.js
+++ config/config_general.js
@@ -8,7 +8,7 @@ var config_general = {
 		      'initTrialView',
 		      'initPostTestView',
 		      'initThanksView' ], // the order in which views are shown during the experiment
-    'viewSteps': [1,1,24,1,2,1,1], // how many steps (slides/trials/...) belong to each view
+    'viewSteps': [1,1,24,1,144,1,1], // how many steps (slides/trials/...) belong to each view
 
     // optional settings specific to this experiment
     "expSettings": {
