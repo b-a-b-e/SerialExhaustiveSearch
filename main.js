b1b0b5969646e1386b14d059eebdec7e4f3f341e
michael-franke <mchfranke@gmail.com>
2019-04-03 10:11:01 +0200

    fixed backend server address

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $("document").ready(function() {
         ],
         deploy: {
             experimentID: "49",
-            serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
+            serverAppURL: "https://mcmpact.ikw.uni-osnabrueck.de/babe/api/submit_experiment/",
             deployMethod: "directLink",
             contact_email: "michael.franke@uni-osnabrueck.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
